e0de13e2607fb4338952ba6bbcb0871b918c98bb
marshal <marshalcraft@gmail.com>
2016-05-10 02:19:46 -0400

    forgot file

---
+++ TransportLayerInterface/ServerInfoReadyHold.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "TransportLayerInterface.h"
+
+#ifndef SERVERINFOREADYHOLDHEADER
+#define SERVERINFOREADYHOLDHEADER
+__inline void ServerInfoReadyHold(_In_ TlsConnection::PAuxillaryConnectionStateData pAuxConSt, _In_ WinSockAPI WindowSockets)
+{
+	do
+	{
+		EnterCriticalSection(&pAuxConSt->UnExactData.ServInf.TlsServerInfoLockObj);
+		pAuxConSt->UnExactData.ServInf.InfoReady = WindowSockets.ServerInfReady;
+		EnterCriticalSection(&pAuxConSt->UnExactData.ServInf.TlsServerInfoLockObj);
+	}
+	while (WindowSockets.ServerInfReady != 0x01);
+}
+#endif
